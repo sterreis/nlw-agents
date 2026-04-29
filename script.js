ad90b3caeaaa1a5177a7bca5779a91937a84d20a
Ester Reis <esterreisoliv@gmail.com>
2026-04-29 00:17:03 -0300

    Refactor AI question handling with new model and rules
    Updated the AI question handling to use the new Gemini model and added detailed response guidelines.

--- script.js
+++ script.js
@@ -11,32 +11,59 @@ const markdownToHTML = (text) => {
 }
 
 const perguntarAI = async (question, game, apiKey) => {
-    const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                contents: [{
-                    role: "user",
-                    parts: [{
-                        text: question
-                    }]
-                }]
-            })
-        }
-    )
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+    ## Especialidade
+    Você é um especialista assistente de meta para o jogo ${game}
+
+    ## Tarefa
+    Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, build e dicas
+
+    ## Regras
+    - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+    - Se a pergunta não está relacionada ao jogo, responda com 'Essa pergunta não está relacionada ao jogo'
+    - Considere a data atual ${new Date().toLocaleDateString()}
+    - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+    - Nunca responsda itens que vc não tenha certeza de que existe no patch atual.
+
+    ## Resposta
+    - Economize na resposta, seja direto e responda no máximo 500 caracteres
+    - Responda em markdown
+    - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+    ## Exemplo de resposta
+    pergunta do usuário: Melhor build rengar jungle
+    resposta: A build mais atual é: \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas:**\n\nexemplo de runas\n\n
+
+    ---
+    Aqui está a pergunta do usuário: ${question}
+  `
+
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    // chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
 
     const data = await response.json()
-    console.log("STATUS:", response.status)
-    console.log("DATA:", data)
-
-    if (!data.candidates) {
-        return "Erro na API"
-    }
-
     return data.candidates[0].content.parts[0].text
 }
 
